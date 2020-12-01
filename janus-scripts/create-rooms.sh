890d189ddba5ffe1108b398a473da4ea2dcaa716
Simon Döring <simon.doering@stud.hs-bochum.de>
2020-12-01 22:33:40 +0100

    Apply small adjustments to style of script

--- janus-scripts/create-rooms.sh
+++ janus-scripts/create-rooms.sh
@@ -17,13 +17,13 @@ for ((i=1; i<=$rooms; i++)); do
 	room="room-100$i"
 	if [ -z "$(grep $room $file)" ]; then
 		echo "
-$room: {
-	description = \"VNC $i Camera\"
-	publishers = 1
-	bitrate = 128000
-	fir_freq = 10
-	record = false
-}" >> $file
+			$room: {
+				description = \"VNC $i Camera\"
+				publishers = 1
+				bitrate = 128000
+				fir_freq = 10
+				record = false
+			}" | sed -re 's/^\t{3}//' >> $file
 		echo "Added entry for $room to $file"
 	else
 		echo "Entry for $room already in $file"
