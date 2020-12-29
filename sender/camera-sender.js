fcd226905d21b90821d63d40bd7d138a548bc7b3
Simon Döring <simon.doering@stud.hs-bochum.de>
2020-12-29 13:18:01 +0100

    Change sender room to be selected through query string

--- sender/camera-sender.js
+++ sender/camera-sender.js
@@ -1,20 +1,18 @@
-var server = 'https://' + window.location.hostname + ':8089/janus';
-
-var janus = null;
-var videoroomHandle = null;
-var room = 1006;
-var sendResolution = 'stdres';
-
-var startButton = null;
-var stopButton = null;
-
-// const socketNumber = room + 4000;
-const socketNumber = 5006;
-const socket = io('https://' + window.location.hostname, { path: '/socket.io/' + socketNumber });
-
 document.addEventListener('DOMContentLoaded', function() {
-    startButton = document.getElementById('start');
-    stopButton = document.getElementById('stop');
+    var server = 'https://' + window.location.hostname + ':8089/janus';
+
+    var janus = null;
+    var videoroomHandle = null;
+    var room = 1006;
+    var sendResolution = 'stdres';
+
+    const socketNumber = room + 4000;
+    const socket = io('https://' + window.location.hostname, { path: '/socket.io/' + socketNumber });
+
+    var startButton = document.getElementById('start');
+    var stopButton = document.getElementById('stop');
+
+    parseRoomFromURL();
 
     Janus.init({ debug: 'all', callback: function() {
         if (!Janus.isWebrtcSupported()) {
@@ -32,17 +30,14 @@ document.addEventListener('DOMContentLoaded', function() {
                         Janus.log('Plugin attached! (' + videoroomHandle.getPlugin() + ', id=' + videoroomHandle.getId() + ')');    
 
                         startButton.onclick = function() {
-                            var roomSelect = document.getElementById('room-select');
                             var resSelect = document.getElementById('res-select');
                             var pinInput = document.getElementById('pin-input');
                             startButton.setAttribute('disabled', '');
-                            roomSelect.setAttribute('disabled', '');
                             resSelect.setAttribute('disabled', '');
                             stopButton.removeAttribute('disabled');
                             stopButton.onclick = function() {
                                 janus.destroy();
                             };
-                            room = parseInt(roomSelect.value);
                             sendResolution = resSelect.value;
                             Janus.log('sendResolution:', sendResolution);
                             shareCamera(pinInput.value);
@@ -70,8 +65,6 @@ document.addEventListener('DOMContentLoaded', function() {
                         if (document.getElementById('camera-preview') == null) {
                             var video = document.createElement('video');
                             video.setAttribute('id', 'camera-preview');
-                            video.setAttribute('width', '100%');
-                            video.setAttribute('height', '100%');
                             video.setAttribute('autoplay', '');
                             video.setAttribute('playsinline', '');
                             video.setAttribute('muted', 'muted');
@@ -97,73 +90,88 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }});
-}, false);
 
-function shareCamera(pin) {
-    var register = {
-        request: 'join',
-        room,
-        ptype: 'publisher',
-        pin
-    };
-    videoroomHandle.send({ message: register });
-}
-
-function handleBandwidthFormSubmit(event) {
-    event.preventDefault();
-    var bandwidthInput = document.getElementById('bandwidth-input');
-    var bitrateStr = bandwidthInput.value.trim();
-    if (bitrateStr !== '' && !isNaN(bitrateStr) ) {
-        var bitrate = parseInt(bitrateStr) * 1000;
-        if (bitrate < 0) {
-            bandwidth = 0;
-            Janus.log('Negative bitrate input set to 0 (unlimited)');
-        }
-        videoroomHandle.send({ message: { request: 'configure', bitrate }});
-        bandwidthInput.value = '';
-    } else {
-        alert('Invalid value for bitrate');
+    function shareCamera(pin) {
+        var register = {
+            request: 'join',
+            room,
+            ptype: 'publisher',
+            pin
+        };
+        videoroomHandle.send({ message: register });
     }
-}
 
-function handleMessage(msg, jsep) {
-    var event = msg['videoroom'];
-    if (event) {
-        if (event === 'joined') {
-            Janus.log('Joined event:', msg);
-            Janus.log('Successfully joined room ' + msg['room'] + ' with ID ' + msg['id']);
-            if (msg['publishers'].length === 0) {
-                videoroomHandle.createOffer({
-                    media: {
-                        videoSend: true,
-                        video: sendResolution,
-                        audioSend: false,
-                        videoRecv: false
-                    },
-                    success: function(jsep) {
-                        var publish = {
-                            request: 'configure',
-                            audio: false,
-                            video: true
-                        };
-                        videoroomHandle.send({ message: publish, jsep });
-                    },
-                    error: function(error) {
-                        Janus.error('WebRTC error:', error);
-                        alert('WebRTC error: ' + error.message);
-                    }
-                });
-            } else {
-                alert('There is already somebody who is sharing his camera in this room!');
+    function handleBandwidthFormSubmit(event) {
+        event.preventDefault();
+        var bandwidthInput = document.getElementById('bandwidth-input');
+        var bitrateStr = bandwidthInput.value.trim();
+        if (bitrateStr !== '' && !isNaN(bitrateStr) ) {
+            var bitrate = parseInt(bitrateStr) * 1000;
+            if (bitrate < 0) {
+                bandwidth = 0;
+                Janus.log('Negative bitrate input set to 0 (unlimited)');
+            }
+            videoroomHandle.send({ message: { request: 'configure', bitrate }});
+            bandwidthInput.value = '';
+        } else {
+            alert('Invalid value for bitrate');
+        }
+    }
+
+    function handleMessage(msg, jsep) {
+        var event = msg['videoroom'];
+        if (event) {
+            if (event === 'joined') {
+                Janus.log('Joined event:', msg);
+                Janus.log('Successfully joined room ' + msg['room'] + ' with ID ' + msg['id']);
+                if (msg['publishers'].length === 0) {
+                    videoroomHandle.createOffer({
+                        media: {
+                            videoSend: true,
+                            video: sendResolution,
+                            audioSend: false,
+                            videoRecv: false
+                        },
+                        success: function(jsep) {
+                            var publish = {
+                                request: 'configure',
+                                audio: false,
+                                video: true
+                            };
+                            videoroomHandle.send({ message: publish, jsep });
+                        },
+                        error: function(error) {
+                            Janus.error('WebRTC error:', error);
+                            alert('WebRTC error: ' + error.message);
+                        }
+                    });
+                } else {
+                    alert('There is already somebody who is sharing his camera in this room!');
+                    window.location.reload();
+                }
+            }
+            if (event === 'event' && msg['error']) {
+                alert('Error message: ' + msg['error'] + '.\nError object: ' + JSON.stringify(msg, null, 2));
                 window.location.reload();
             }
         }
-        if (event === 'event' && msg['error']) {
-            alert('Error message: ' + msg['error'] + '.\nError object: ' + JSON.stringify(msg, null, 2));
-            window.location.reload();
+        if (jsep) {
+            videoroomHandle.handleRemoteJsep({ jsep });
         }
+    };
+
+    function parseRoomFromURL() {
+        var urlParams = new URLSearchParams(window.location.search);
+        var roomParam = urlParams.get('room');
+        var roomIndicator = document.getElementById('room-indicator');
+        var appendix = '';
+        if (roomParam != null && !isNaN(roomParam)) {
+            room = parseInt(roomParam);
+        } else {
+            console.log('Got no valid room in URL search params, using default room ' + room);
+            appendix = ' (Default value)';
+        }
+        roomIndicator.innerText = `VNC ${room - 1000} - Room ${room}` + appendix;
     }
-    if (jsep) {
-        videoroomHandle.handleRemoteJsep({ jsep });
-    }
-};
+}, false);
+
